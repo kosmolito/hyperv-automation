c91ed6c777baa7568766caf02da11bd25240bd4c
kosmolito <said.karker@gmail.com>
2022-11-25 12:31:04 +0100

    Ability to add domain name

--- generate-random-users.ps1
+++ generate-random-users.ps1
@@ -1,12 +1,14 @@
-﻿$RandomNameList = Import-Csv "$PSScriptRoot\random-names.csv"
-$UserAmount = Read-Host -Prompt "How many users are to be created?"
-$UserPassword = Read-Host -Prompt "Insert the password for the users:"
-$RawOU = Read-Host -Prompt "Insert the name of the OUs separated by a comma:"
-$OU = $RawOU -split ","
-$RAWSecurityGroups = Read-Host -Prompt "Insert security groups separated by a comma:"
-$SecurityGroup = $RAWSecurityGroups -split ","
-$RandomCsvFilePath = "$PSScriptRoot\example-resource\random-generated-users.csv"
+﻿$RandomNameList = Import-Csv "$PSScriptRoot\example-resource\random-names.csv"
+[ValidateRange(1, 500)]$UserAmount = Read-Host -Prompt "How many users are to be created? (Max 500)"
+$DomainName = Read-Host -Prompt "Insert the UPN/Domain eg. mstile.se"
+$UserPassword = Read-Host -Prompt "Insert the password for the users"
+$RAWGeneralSecurityGroup = Read-Host -Prompt "Insert general security group name, eg. Public"
+$RAWSecurityGroups = Read-Host -Prompt "Insert other security groups separated by a comma, eg. Sales,Ekonomi"
+$SecurityGroup = $RAWSecurityGroups.split(",")
+$RawOU = Read-Host -Prompt "Insert the name of the OUs separated by a comma eg. site1_users,site2_users"
+$OU = $RawOU.split(",")
 
+$RandomCsvFilePath = "$PSScriptRoot\random-generated-users.csv"
 if (test-path ($RandomCsvFilePath)) {
 Remove-Item -Path $RandomCsvFilePath
 }
@@ -19,8 +21,11 @@ for ($i = 0; $i -lt $UserAmount; $i++) {
 		[PsCustomObject]@{
 		FirstName = $RandomNameList.FirstName[$RandomFirstName]
 		LastName = $RandomNameList.LastName[$RandomLastName]
+		DomainName = $DomainName
 		UserPassword = $UserPassword
-		SecurityGroups = $SecurityGroup[$RandomSecurityGroup]
+		SecurityGroups = "SEC_$($RAWGeneralSecurityGroup)" + "," + "SEC_$($SecurityGroup[$RandomSecurityGroup])"
 		OU = $OU[$RandomOU]
 		} | Export-Csv -Path $RandomCsvFilePath -Append -Encoding ASCII
+	$UserList = import-csv -Path $RandomCsvFilePath
 	}
+$UserList = Import-Csv -Path $RandomCsvFilePath
