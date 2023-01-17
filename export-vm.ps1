2d8e4f1bb725c056f7da8213a016e3839588a8bf
kosmolito <said.karker@gmail.com>
2023-01-17 22:52:57 +0100

    Option to exit or go back

--- export-vm.ps1
+++ export-vm.ps1
@@ -3,3 +3,11 @@ Clear-Host
 Write-Host "VM to export" -ForegroundColor Red
 $ExistingVMList = Get-VM
 $ExistingVMList | ForEach-Object {$index=0} {$_;$index++} | Format-Table -Property @{Label="Index";Expression={$index}},Name,State
+
+$VMSelected = Read-Host "Chose VM to export, multiple vm can be selected by coma separation`nex. 1,3 (b) for back, (e) for exit"
+if ($VMSelected -like "b") {
+    Invoke-Script -ScriptItem Main -PauseBefore $false
+    exit
+} elseif ($VMSelected -like "e") {
+    exit
+}
