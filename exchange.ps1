c3eff6b4a7d682320a1f5f515ef570ee530e952e
kosmolito <said.karker@gmail.com>
2023-02-15 15:56:00 +0100

    Dynamic Creds if vm is joined to domain or not

--- exchange.ps1
+++ exchange.ps1
@@ -1,3 +1,9 @@
 . .\Variables.ps1
 Clear-Host
 $VM = $VMList | Where-Object {$_.isSelected -eq $true -and $_.Roles -contains "Exchange"}
+
+if ($VM.HasJoinedDomain -eq $false) {
+    $Credential = $ServerLocalCredential
+} else {
+    $Credential = $DomainCredential
+}
